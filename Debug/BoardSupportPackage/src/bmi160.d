8d6f23860a679e1c986a9044dd2a6fecfe1984cd
unknown <daniel.labes@ufl.edu>
2020-08-18 19:23:09 -0400

    Added a .gitignore

--- Debug/BoardSupportPackage/src/bmi160.d
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-BoardSupportPackage/src/bmi160.obj: ../BoardSupportPackage/src/bmi160.c
-BoardSupportPackage/src/bmi160.obj: C:/Users/Danny/workspace_v9/lab5/BoardSupportPackage/inc/bmi160.h
-
-../BoardSupportPackage/src/bmi160.c: 
-C:/Users/Danny/workspace_v9/lab5/BoardSupportPackage/inc/bmi160.h: 
